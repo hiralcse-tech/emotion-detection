f2fe8e028492863034a2f2f7d4295c9ff34f036c
hiralcse-tech <hiral.cse@gmail.com>
2026-03-29 23:36:32 +0530

    app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ import mediapipe as mp
 # -----------------------------
 # CONFIG
 # -----------------------------
-st.set_page_config(page_title="Emotion AI + Explainable AI", layout="centered")
-st.title("😊 Emotion Detection + Grad-CAM")
+st.set_page_config(page_title="Emotion AI + Grad-CAM", layout="centered")
+st.title("😊 Emotion Detection + Explainable AI")
 
 classes = ['angry', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 MODEL_PATH = "emotion_model.pth"
@@ -59,14 +59,11 @@ transform = transforms.Compose([
 ])
 
 # -----------------------------
-# FACE DETECTOR
+# FACE DETECTOR + ALIGNMENT
 # -----------------------------
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 mp_face_mesh = mp.solutions.face_mesh
 
-# -----------------------------
-# FACE ALIGNMENT
-# -----------------------------
 def align_face(image, box):
     x, y, w, h = box
     face = image[y:y+h, x:x+w]
@@ -76,8 +73,8 @@ def align_face(image, box):
         results = face_mesh.process(img_rgb)
         if results.multi_face_landmarks:
             lm = results.multi_face_landmarks[0].landmark
-            left_eye = lm[33]  # approx left eye
-            right_eye = lm[263]  # approx right eye
+            left_eye = lm[33]
+            right_eye = lm[263]
             dy = right_eye.y - left_eye.y
             dx = right_eye.x - left_eye.x
             angle = np.arctan2(dy, dx) * 180 / np.pi
