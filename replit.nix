46b083ed1b2af5645639872265792725deb08995
hiralcse-tech <hiral.cse@gmail.com>
2026-04-02 22:39:19 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,10 +2,8 @@
   deps = [
     pkgs.python311
     pkgs.python311Packages.pip
-    pkgs.python311Packages.virtualenv
+    pkgs.python311Packages.streamlit
     pkgs.opencv4
-    pkgs.ffmpeg
-    pkgs.stdenv.cc.cc.lib
     pkgs.libGL
     pkgs.libGLU
   ];
